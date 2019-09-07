499fb18a48c292fdcc54edf9e0671a5795b13ecd
Alex Li <5796630@mail.ru>
2019-09-07 05:07:55 +0300

    sntp

--- SOURCE/src/w5500/sntp.c
+++ SOURCE/src/w5500/sntp.c
@@ -5,33 +5,27 @@ ntpformat NTPformat;
 uint8_t ntpmessage[48];
 uint8_t *data_buf;
 uint8_t NTP_SOCKET;
-uint8_t time_zone;
-uint16_t ntp_retry_cnt=0; //counting the ntp retry number
+uint16_t ntp_retry_cnt = 0x00; //counting the ntp retry number
 
 void SNTP_init(uint8_t s, uint8_t *ntp_server, uint8_t tz, uint8_t *buf){
+  CopyBeffer(NTPformat.dstaddr, ntp_server, 0x00, 0x04);
   NTP_SOCKET = s;
-
-  NTPformat.dstaddr[0x00] = ntp_server[0x00];
-  NTPformat.dstaddr[0x01] = ntp_server[0x01];
-  NTPformat.dstaddr[0x02] = ntp_server[0x02];
-  NTPformat.dstaddr[0x03] = ntp_server[0x03];
-
   data_buf = buf;
 
   uint8_t Flag;
-  NTPformat.leap = 0;           /* leap indicator */
-  NTPformat.version = 4;        /* version number */
-  NTPformat.mode = 3;           /* mode */
-  NTPformat.stratum = 0;        /* stratum */
-  NTPformat.poll = 0;           /* poll interval */
-  NTPformat.precision = 0;      /* precision */
-  NTPformat.rootdelay = 0;      /* root delay */
-  NTPformat.rootdisp = 0;       /* root dispersion */
-  NTPformat.refid = 0;          /* reference ID */
-  NTPformat.reftime = 0;        /* reference time */
-  NTPformat.org = 0;            /* origin timestamp */
-  NTPformat.rec = 0;            /* receive timestamp */
-  NTPformat.xmt = 1;            /* transmit timestamp */
+  NTPformat.leap =      0x00;      /* leap indicator */
+  NTPformat.version =   0x04;      /* version number */
+  NTPformat.mode =      0x03;      /* mode */
+  NTPformat.stratum =   0x00;      /* stratum */
+  NTPformat.poll =      0x00;      /* poll interval */
+  NTPformat.precision = 0x00;      /* precision */
+  NTPformat.rootdelay = 0x00;      /* root delay */
+  NTPformat.rootdisp =  0x00;      /* root dispersion */
+  NTPformat.refid =     0x00;      /* reference ID */
+  NTPformat.reftime =   0x00;      /* reference time */
+  NTPformat.org =       0x00;      /* origin timestamp */
+  NTPformat.rec =       0x00;      /* receive timestamp */
+  NTPformat.xmt =       0x01;      /* transmit timestamp */
 
   Flag = (NTPformat.leap << 0x06) + (NTPformat.version << 0x03) + NTPformat.mode; //one byte Flag
   memcpy(ntpmessage,(void const*)(&Flag), true);
@@ -41,15 +35,15 @@ int8_t SNTP_run(void){
   uint16_t RSR_len;
   uint32_t destip = 0x00;
   uint16_t destport;
-  uint16_t startindex = 40; //last 8-byte of data_buf[size is 48 byte] is xmt, so the startindex should be 40
+  uint16_t startindex = 0x28; //last 8-byte of data_buf[size is 48 byte] is xmt, so the startindex should be 40
   uint32_t seconds = 0x00;
   switch(getSn_SR(NTP_SOCKET)){
     case SOCK_UDP:
-      if((RSR_len = getSn_RX_RSR(NTP_SOCKET)) > 0){
+      if((RSR_len = getSn_RX_RSR(NTP_SOCKET)) > 0x00){
         if(RSR_len > MAX_SNTP_BUF_SIZE) RSR_len = MAX_SNTP_BUF_SIZE; // if Rx data size is lager than TX_RX_MAX_BUF_SIZE
         recvfrom(NTP_SOCKET, data_buf, RSR_len, (uint8_t *)&destip, &destport);
-        for(uint8_t i = 0; i < 4; i++){ seconds = (seconds << 8) | data_buf[startindex + i]; }
-        seconds -= 2208988800;
+        for(uint8_t i = 0x00; i < 0x04; i++){ seconds = (seconds << 0x08) | data_buf[startindex + i]; }
+        seconds -= 0x83AA7E80; //2208988800;
         RtcSetSeconds(seconds);
         RtcTimeStamp();
         printf("\r\n");
@@ -62,7 +56,7 @@ int8_t SNTP_run(void){
           sendto(NTP_SOCKET, ntpmessage, sizeof(ntpmessage), NTPformat.dstaddr, ntp_port);
           ntp_retry_cnt++;
         }else{ // send request again? it should wait for a while
-          if((ntp_retry_cnt % 0xFFF) == false){ //wait time
+          if((ntp_retry_cnt % 0x0FFF) == false){ //wait time
             sendto(NTP_SOCKET, ntpmessage, sizeof(ntpmessage), NTPformat.dstaddr, ntp_port);
             #ifdef DEBUG_SNTP
               printf("ntp retry: %d\r\n", ntp_retry_cnt);
@@ -79,7 +73,7 @@ int8_t SNTP_run(void){
       }
     break;
     case SOCK_CLOSED:
-      socket(NTP_SOCKET,Sn_MR_UDP,ntp_port,0);
+      socket(NTP_SOCKET, Sn_MR_UDP, ntp_port, false);
     break;
   }
   return false; // 0 - failed / 1 - success
